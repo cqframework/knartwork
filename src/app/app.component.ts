9cec1479eecb54a902613d9781742ed3d082539d
frostbytedata <contact@frostbytedata.com>
2018-05-24 21:50:02 -0400

    Adding simple vanilla ES6 object class to send events.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+import ces, { EventModel } from './services/ces';
 @Component({
   selector: 'app',
   template: '<router-outlet></router-outlet>',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
   constructor() {
     console.log("AppComponent has been initialized to establish router element.");
+  }
+  ngOnInit(){
+    let event = {
+      person_id:"test",
+      topic_uri:"http://www.ke.tu-darmstadt.de/ontologies/ui_detail_level.owl#mouse-single-click",
+      model_uri:"",
+      agent_uri:"artaka://agents/knartwork",
+      parameters: {anything : "some data"}
+    };
+    ces.send(event).then((response) => {
+      console.info(response);
+    });
   }
 }
