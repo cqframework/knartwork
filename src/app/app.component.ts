5ce654d7f94d0191f0173fda56f0ce0c418adc27
Paul Williams <paulwilliams0@gmail.com>
2018-06-14 16:32:10 -0500

    Client side `document:selectionchange` added. TODO: configure ces event correctly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { CESService, ActionEvent, eventFilter } from "context-event-client";
 import { Observable } from "rxjs/Observable";
 import { Subject } from "rxjs/Subject";
 import 'rxjs/add/operator/filter';
+
+export function debounce(delay: number = 300): MethodDecorator {
+  return function(target: any, propertyKey: string, descriptor: 
+    PropertyDescriptor) {
+      let timeout = null;
+
+      const original = descriptor.value;
+
+      descriptor.value = function (...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => original.apply(this,args), delay);
+      };
+
+      return descriptor;
+    };
+}
+
 @Component({
   selector: 'app',
   template: '<router-outlet></router-outlet>',
@@ -13,6 +30,21 @@ export class AppComponent implements OnInit {
   constructor(private ces: CESService) {
     console.log("AppComponent has been initialized to establish router element.");
   }
+  @HostListener('document:selectionchange', ['$event'])
+    @debounce()
+    onSelectionChange(ev:MouseEvent) {
+      const sel = document.getSelection()
+      const selectedText = sel.rangeCount ? sel.getRangeAt(0).toString() : null
+      
+      if(selectedText) {
+        console.log("selectedText:", selectedText);
+        // TODO: 
+        // this.ces.send()
+        // http://www.ke.tu-darmstadt.de/ontologies/ui_detail_level.owl#select-value
+        // { selectedText: selectedText }
+      }
+    }
+
   ngOnInit(){
     this.ces.initialize(['artaka://ticks/second']);
     this.ces.getEventStream()
