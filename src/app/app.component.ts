b892b04f1a63c69a746c59ea5613c73ca77eecfe
frostbytedata <contact@frostbytedata.com>
2018-06-19 13:17:55 -0400

    Adding toaster message when event is received.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { CESService, ActionEvent, eventFilter } from "context-event-client";
 import { Observable } from "rxjs/Observable";
 import { Subject } from "rxjs/Subject";
 import 'rxjs/add/operator/filter';
-
+import {ToasterModule, ToasterService} from 'angular2-toaster/angular2-toaster';
 @Component({
   selector: 'app',
   template: '<router-outlet></router-outlet>',
@@ -11,18 +11,20 @@ import 'rxjs/add/operator/filter';
 })
 export class AppComponent implements OnInit {
   title = 'app';
-  constructor(private ces: CESService) {
+  constructor(private ces: CESService,
+              public toasterService: ToasterService,) {
     console.log("AppComponent has been initialized to establish router element.");
   }
 
   ngOnInit(){
-    this.ces.initialize(['http://www.ke.tu-darmstadt.de/ontologies/ui_detail_level.owl#select-value']);
+    this.ces.initialize(['http://www.ke.tu-darmstadt.de/ontologies/ui_detail_level.owl#select-value', 'artaka://ticks/second']);
     this.ces.getEventStream()
       // Here we can use any RxJs operators!
       .filter((event) => {
-        return eventFilter(['select-value*'], event);
+        return eventFilter(['artaka*', 'select-value*'], event);
       })
       .subscribe((event)=>{
+        this.toasterService.pop("success", "Context event Received", event.model_uri);
         console.info('Event received: ', event);
     });
 
